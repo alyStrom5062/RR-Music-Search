bc15996028777d1edfe3871e3a95a800d7dd6b8d
alyStrom5062 <Alyenastrom00@gmail.com>
2023-07-15 18:35:47 -0700

    Test

---
+++ src/components/ArtistView.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+function ArtistView() {
+    const [ artistData, setArtistData ] = useState([])
+
+    return (
+        <div>
+            <p>Artist Data Goes Here!</p>
+        </div>
+    )
+}
+
+export default ArtistView;
